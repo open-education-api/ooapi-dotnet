eb6756caa941292c62314303540197cc8dc63252
ramiro.diniz@inholland.nl <ramiro.diniz@inholland.nl>
2022-10-03 16:29:53 +0200

    Introducing global enum LevelEnum and use it in Course and EducationSpecification. Rename filename (typo)

---
+++ v5/OOAPIV5GENERATED/src/IO.Swagger/Enums/LevelEnum.cs
@@ -0,0 +1,74 @@
+﻿using System.Runtime.Serialization;
+
+namespace IO.Swagger.Enums
+{
+
+    public enum LevelEnum
+    {
+        /// <summary>
+        /// Enum SecondaryVocationalEducationEnum for secondary vocational education
+        /// </summary>
+        [EnumMember(Value = "secondary vocational education")]
+        SecondaryVocationalEducationEnum = 0,
+        /// <summary>
+        /// Enum SecondaryVocationalEducation1Enum for secondary vocational education 1
+        /// </summary>
+        [EnumMember(Value = "secondary vocational education 1")]
+        SecondaryVocationalEducation1Enum = 1,
+        /// <summary>
+        /// Enum SecondaryVocationalEducation2Enum for secondary vocational education 2
+        /// </summary>
+        [EnumMember(Value = "secondary vocational education 2")]
+        SecondaryVocationalEducation2Enum = 2,
+        /// <summary>
+        /// Enum SecondaryVocationalEducation3Enum for secondary vocational education 3
+        /// </summary>
+        [EnumMember(Value = "secondary vocational education 3")]
+        SecondaryVocationalEducation3Enum = 3,
+        /// <summary>
+        /// Enum SecondaryVocationalEducation4Enum for secondary vocational education 4
+        /// </summary>
+        [EnumMember(Value = "secondary vocational education 4")]
+        SecondaryVocationalEducation4Enum = 4,
+        /// <summary>
+        /// Enum AssociateDegreeEnum for associate degree
+        /// </summary>
+        [EnumMember(Value = "associate degree")]
+        AssociateDegreeEnum = 5,
+        /// <summary>
+        /// Enum BachelorEnum for bachelor
+        /// </summary>
+        [EnumMember(Value = "bachelor")]
+        BachelorEnum = 6,
+        /// <summary>
+        /// Enum MasterEnum for master
+        /// </summary>
+        [EnumMember(Value = "master")]
+        MasterEnum = 7,
+        /// <summary>
+        /// Enum DoctoralEnum for doctoral
+        /// </summary>
+        [EnumMember(Value = "doctoral")]
+        DoctoralEnum = 8,
+        /// <summary>
+        /// Enum UndefinedEnum for undefined
+        /// </summary>
+        [EnumMember(Value = "undefined")]
+        UndefinedEnum = 9,
+        /// <summary>
+        /// Enum UndividedEnum for undivided
+        /// </summary>
+        [EnumMember(Value = "undivided")]
+        UndividedEnum = 10,
+        /// <summary>
+        /// Enum Nt21Enum for nt2-1
+        /// </summary>
+        [EnumMember(Value = "nt2-1")]
+        Nt21Enum = 11,
+        /// <summary>
+        /// Enum Nt22Enum for nt2-2
+        /// </summary>
+        [EnumMember(Value = "nt2-2")]
+        Nt22Enum = 12
+    }
+}
