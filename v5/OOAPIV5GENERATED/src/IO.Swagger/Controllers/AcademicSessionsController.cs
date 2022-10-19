975f7a445248aaa3b7d076a418cf46fa40c9c816
ramiro.diniz@inholland.nl <ramiro.diniz@inholland.nl>
2022-10-19 18:35:17 +0200

    Added some attributes, removed export-comments in models, some other clean-ups

--- v5/OOAPIV5GENERATED/src/IO.Swagger/Controllers/AcademicSessionsController.cs
+++ v5/OOAPIV5GENERATED/src/IO.Swagger/Controllers/AcademicSessionsController.cs
@@ -18,6 +18,54 @@ namespace IO.Swagger.Controllers
     public class AcademicSessionsController : BaseController
     {
         /// <summary>
+        /// GET /academic-sessions
+        /// </summary>
+        /// <remarks>Get a list of all academic sessions, ordered chronologically.</remarks>
+        /// <param name="primaryCodeModel"></param>
+        /// <param name="filterModel"></param>
+        /// <param name="pagingModel"></param>
+        /// <param name="academicSessionType">Filter by academic session type <br/> Example: academicSessionType=semester</param>
+        /// <param name="parent">Filter by parent (academicSessionId)</param>
+        /// <param name="year">Filter by year (academicSessionId)</param>
+        /// <param name="sort">	
+        /// Array of strings <br/>
+        /// Default: ["startDate"] <br/>
+        /// Items Enum: "academicSessionId" "name" "startDate" "-academicSessionId" "-name" "-startDate" <br/>
+        /// Example: sort=startDate,-academicSessionId <br/>
+        /// Sort by one or more attributes, the default is ascending. Prefixing the attribute with a minus sign - allows for descending sort. Examples: [ATTR | -ATTR | ATTR1,-ATTR2]</param>
+        /// <response code="200">OK</response>
+        [HttpGet]
+        [Route("academic-sessions")]
+        [ValidateModelState]
+        [SwaggerOperation("AcademicSessionsGet")]
+        [SwaggerResponse(statusCode: 200, type: typeof(List<AcademicSession>), description: "OK")]
+        public virtual IActionResult AcademicSessionsGet([FromQuery] PrimaryCodeModel primaryCodeModel, [FromQuery] FilterModel filterModel, [FromQuery] PagingModel pagingModel, [FromQuery] string academicSessionType, [FromQuery] Guid? parent, [FromQuery] Guid? year, [FromQuery] string sort)
+        {
+            //TODO: Uncomment the next line to return response 200 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(200, default(InlineResponse20013));
+
+            //TODO: Uncomment the next line to return response 400 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(400, default(InlineResponse400));
+
+            //TODO: Uncomment the next line to return response 401 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(401, default(InlineResponse400));
+
+            //TODO: Uncomment the next line to return response 403 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(403, default(InlineResponse400));
+
+            //TODO: Uncomment the next line to return response 405 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(405, default(InlineResponse400));
+
+            //TODO: Uncomment the next line to return response 429 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(429, default(InlineResponse400));
+
+            //TODO: Uncomment the next line to return response 500 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(500, default(InlineResponse400));
+
+            return null;
+        }
+
+        /// <summary>
         /// GET /academic-sessions/{academicSessionId}
         /// </summary>
         /// <remarks>Get a single academic session.</remarks>
@@ -93,54 +141,6 @@ namespace IO.Swagger.Controllers
 
             //TODO: Uncomment the next line to return response 404 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
             // return StatusCode(404, default(InlineResponse400));
-
-            //TODO: Uncomment the next line to return response 405 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
-            // return StatusCode(405, default(InlineResponse400));
-
-            //TODO: Uncomment the next line to return response 429 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
-            // return StatusCode(429, default(InlineResponse400));
-
-            //TODO: Uncomment the next line to return response 500 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
-            // return StatusCode(500, default(InlineResponse400));
-
-            return null;
-        }
-
-        /// <summary>
-        /// GET /academic-sessions
-        /// </summary>
-        /// <remarks>Get a list of all academic sessions, ordered chronologically.</remarks>
-        /// <param name="primaryCodeModel"></param>
-        /// <param name="filterModel"></param>
-        /// <param name="pagingModel"></param>
-        /// <param name="academicSessionType">Filter by academic session type <br/> Example: academicSessionType=semester</param>
-        /// <param name="parent">Filter by parent (academicSessionId)</param>
-        /// <param name="year">Filter by year (academicSessionId)</param>
-        /// <param name="sort">	
-        /// Array of strings <br/>
-        /// Default: ["startDate"] <br/>
-        /// Items Enum: "academicSessionId" "name" "startDate" "-academicSessionId" "-name" "-startDate" <br/>
-        /// Example: sort=startDate,-academicSessionId <br/>
-        /// Sort by one or more attributes, the default is ascending. Prefixing the attribute with a minus sign - allows for descending sort. Examples: [ATTR | -ATTR | ATTR1,-ATTR2]</param>
-        /// <response code="200">OK</response>
-        [HttpGet]
-        [Route("academic-sessions")]
-        [ValidateModelState]
-        [SwaggerOperation("AcademicSessionsGet")]
-        [SwaggerResponse(statusCode: 200, type: typeof(List<AcademicSession>), description: "OK")]
-        public virtual IActionResult AcademicSessionsGet([FromQuery] PrimaryCodeModel primaryCodeModel, [FromQuery] FilterModel filterModel, [FromQuery] PagingModel pagingModel, [FromQuery] string academicSessionType, [FromQuery] Guid? parent, [FromQuery] Guid? year, [FromQuery] string sort)
-        {
-            //TODO: Uncomment the next line to return response 200 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
-            // return StatusCode(200, default(InlineResponse20013));
-
-            //TODO: Uncomment the next line to return response 400 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
-            // return StatusCode(400, default(InlineResponse400));
-
-            //TODO: Uncomment the next line to return response 401 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
-            // return StatusCode(401, default(InlineResponse400));
-
-            //TODO: Uncomment the next line to return response 403 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
-            // return StatusCode(403, default(InlineResponse400));
 
             //TODO: Uncomment the next line to return response 405 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
             // return StatusCode(405, default(InlineResponse400));
