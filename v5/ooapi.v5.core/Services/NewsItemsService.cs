ca26dd7751dff96bfe148786f1016437a7fd7b28
ramiro.diniz@inholland.nl <ramiro.diniz@inholland.nl>
2022-12-14 17:18:28 +0100

    Endpoints GET /news-feeds/{newsFeedId}/news-items en GET /news-items/{newsItemId}

---
+++ v5/ooapi.v5.core/Services/NewsItemsService.cs
@@ -0,0 +1,49 @@
+﻿using ooapi.v5.core.Models;
+using ooapi.v5.core.Repositories;
+using ooapi.v5.core.Utility;
+using ooapi.v5.Models;
+
+namespace ooapi.v5.core.Services
+{
+    public class NewsItemsService : ServiceBase
+    {
+        private readonly NewsItemsRepository _repository;
+
+        public NewsItemsService(CoreDBContext dbContext, UserRequestContext userRequestContext) : base(dbContext, userRequestContext)
+        {
+            _repository = new NewsItemsRepository(dbContext);
+        }
+
+        public NewsItem Get(Guid newsitemId, out ErrorResponse errorResponse)
+        {
+            try
+            {
+                var item = _repository.GetNewsItem(newsitemId);
+
+                errorResponse = null;
+                return item;
+            }
+            catch (Exception ex)
+            {
+                errorResponse = new ErrorResponse(500);
+                return null;
+            }
+        }
+
+        public Pagination<NewsItem> GetNewsItemsByNewsFeedId(DataRequestParameters dataRequestParameters, Guid newsfeedId, out ErrorResponse errorResponse)
+        {
+            try
+            {
+                var result = _repository.GetNewsItemsByNewsFeedId(newsfeedId);
+                var paginationResult = new Pagination<NewsItem>(result.AsQueryable(), dataRequestParameters);
+                errorResponse = null;
+                return paginationResult;
+            }
+            catch (Exception ex)
+            {
+                errorResponse = new ErrorResponse(500);
+                return null;
+            }
+        }
+    }
+}
--- v5/ooapi.v5/Controllers/NewsController.cs
+++ v5/ooapi.v5/Controllers/NewsController.cs
@@ -99,31 +99,14 @@ public class NewsController : BaseController
     [SwaggerResponse(statusCode: 200, type: typeof(NewsItems), description: "OK")]
     public virtual IActionResult NewsFeedsNewsFeedIdNewsItemsGet([FromRoute][Required] Guid newsFeedId, [FromQuery] FilterParams filterParams, [FromQuery] PagingParams pagingParams, [FromQuery] string? author, [FromQuery] string sort = "newsItemId")
     {
-        //TODO: Uncomment the next line to return response 200 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
-        // return StatusCode(200, default(InlineResponse20031));
-
-        //TODO: Uncomment the next line to return response 400 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
-        // return StatusCode(400, default(InlineResponse400));
-
-        //TODO: Uncomment the next line to return response 401 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
-        // return StatusCode(401, default(InlineResponse400));
-
-        //TODO: Uncomment the next line to return response 403 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
-        // return StatusCode(403, default(InlineResponse400));
-
-        //TODO: Uncomment the next line to return response 404 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
-        // return StatusCode(404, default(InlineResponse400));
-
-        //TODO: Uncomment the next line to return response 405 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
-        // return StatusCode(405, default(InlineResponse400));
-
-        //TODO: Uncomment the next line to return response 429 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
-        // return StatusCode(429, default(InlineResponse400));
-
-        //TODO: Uncomment the next line to return response 500 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
-        // return StatusCode(500, default(InlineResponse400));
-
-        return null;
+        DataRequestParameters parameters = new DataRequestParameters(filterParams, pagingParams, sort);
+        var service = new NewsItemsService(DBContext, UserRequestContext);
+        var result = service.GetNewsItemsByNewsFeedId(parameters, newsFeedId, out ErrorResponse errorResponse);
+        if (result == null)
+        {
+            return BadRequest(errorResponse);
+        }
+        return Ok(result);
     }
 
     /// <summary>
