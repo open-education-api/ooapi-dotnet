ef56587b88a5c221dc392207eeba885bf10201b7
Jeroen Maassen <Jeroen.Maassen@han.nl>
2023-10-13 15:07:15 +0200

    fix bugs

--- v5/ooapi.v5.core/Services/Interfaces/IRoomsService.cs
+++ v5/ooapi.v5.core/Services/Interfaces/IRoomsService.cs
@@ -5,11 +5,8 @@ namespace ooapi.v5.core.Services.Interfaces
 {
     public interface IRoomsService
     {
-        bool Equals(object? obj);
         Room Get(Guid roomId, out ErrorResponse errorResponse);
         Pagination<Room> GetAll(DataRequestParameters dataRequestParameters, out ErrorResponse errorResponse);
-        int GetHashCode();
         Pagination<Room> GetRoomsByBuildingId(DataRequestParameters dataRequestParameters, Guid buildingId, out ErrorResponse errorResponse);
-        string? ToString();
     }
 }
--- v5/ooapi.v5.core/Services/RoomsService.cs
+++ v5/ooapi.v5.core/Services/RoomsService.cs
@@ -61,20 +61,5 @@ namespace ooapi.v5.core.Services
                 return null;
             }
         }
-
-        public override bool Equals(object? obj)
-        {
-            return base.Equals(obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return base.GetHashCode();
-        }
-
-        public override string? ToString()
-        {
-            return base.ToString();
-        }
     }
 }
