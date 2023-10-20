7ffa679622624785e55aedbdf99e4a3432c98baa
Marthijn van den Heuvel <marthijn.vandenheuvel@han.nl>
2023-10-20 10:16:43 +0200

    Add LanguageTypedStringJsonConverter tests

---
+++ v5/ooapi.v5.core.UnitTests/Helpers/LanguageTypedStringJsonConverterTests.cs
@@ -0,0 +1,58 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using Newtonsoft.Json;
+using ooapi.v5.Helpers;
+using ooapi.v5.Models;
+
+namespace ooapi.v5.core.UnitTests.Helpers;
+
+public sealed class LanguageTypedStringJsonConverterTests
+{
+    private readonly IFixture _fixture = new Fixture();
+
+    [TestCase("")]
+    [TestCase(null)]
+    public void GetLanguageTypesStringList_EmptyList_ReturnsEmptyList(string? languages)
+    {
+        // act
+        var result = LanguageTypedStringJsonConverter.GetLanguageTypesStringList(languages);
+
+        // assert
+        result.Should().BeEmpty();
+    }
+
+    [Test]
+    public void GetLanguageTypesStringList_InvalidArray_ReturnsEmptyList()
+    {
+        // arrange
+        var languages = new List<string> { "test" };
+        var json = JsonConvert.SerializeObject(languages);
+
+        // act
+        var result = LanguageTypedStringJsonConverter.GetLanguageTypesStringList(json);
+
+        // assert
+        result.Should().BeEmpty();
+    }
+
+    [Test]
+    public void GetLanguageTypesStringList_ValidLanguages_ReturnsList()
+    {
+        // arrange
+        var languages = new List<LanguageTypedString>
+                            {
+                                new () { Language = "en-GB", Value = _fixture.Create<string>() },
+                                new () { Language = "fr-BE", Value = _fixture.Create<string>() },
+                                new () { Language = "nl-NL", Value = _fixture.Create<string>() },
+                            };
+        var json = JsonConvert.SerializeObject(languages);
+
+        // act
+        var result = LanguageTypedStringJsonConverter.GetLanguageTypesStringList(json);
+
+        // assert
+        result.Should().NotBeNullOrEmpty();
+        result.Count.Should().Be(languages.Count);
+        result.Should().BeEquivalentTo(languages);
+    }
+}
--- v5/ooapi.v5.core/Helpers/LanguageTypedStringJsonConverter.cs
+++ v5/ooapi.v5.core/Helpers/LanguageTypedStringJsonConverter.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json;
 using ooapi.v5.Models;
 
 namespace ooapi.v5.Helpers;
+
 public static class LanguageTypedStringJsonConverter
 {
     internal static List<LanguageTypedString> GetLanguageTypesStringList(string? languageTypedStringValue)
