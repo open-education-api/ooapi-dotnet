dd2ce8518d3527cdf2d2c5858c7413f1ac0aae2b
Marthijn van den Heuvel <marthijn.vandenheuvel@han.nl>
2023-10-20 10:06:51 +0200

    Add DateFormatConverter test

---
+++ v5/ooapi.v5.core.UnitTests/Helpers/DateFormatConverterTests.cs
@@ -0,0 +1,23 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using ooapi.v5.Helpers;
+
+namespace ooapi.v5.core.UnitTests.Helpers;
+
+public sealed class DateFormatConverterTests
+{
+    private readonly IFixture _fixture = new Fixture();
+
+    [Test]
+    public void DateFormatConverter_Constructor_ShouldHaveFormat()
+    {
+        // arrange
+        var format = _fixture.Create<string>();
+
+        // act
+        var result = new DateFormatConverter(format);
+
+        // assert
+        result.DateTimeFormat.Should().Be(format);
+    }
+}
