a12b5dd4b6db39234bcc3193d2f2bbb56bdf54ff
Arjan Verdaasdonk <arjan.verdaasdonk@han.nl>
2023-10-26 16:27:03 +0200

    CoursesController async/await

--- v5/ooapi.v5/Controllers/CoursesController.cs
+++ v5/ooapi.v5/Controllers/CoursesController.cs
@@ -95,60 +95,80 @@ public class CoursesController : BaseController
         return Ok(result);
     }
 
-    /// <summary>
-    /// GET /courses/{courseId}/offerings
-    /// </summary>
-    /// <remarks>Get a list of all offerings for this course, ordered chronologically.</remarks>
-    /// <param name="courseId">Course ID</param>
-    /// <param name="filterParams"></param>
-    /// <param name="pagingParams"></param>
-    /// <param name="teachingLanguage">Filter by teachingLanguage, which is a string describing the main teaching language, should be a three-letter language code as specified by ISO 639-2.</param>
-    /// <param name="modeOfDelivery">Filter by modeOfDelivery</param>
-    /// <param name="resultExpected">Filter by resultExpected</param>
-    /// <param name="since">Filter all offerings by providing a minimum start date for the corresponding academic session, RFC3339 (full-date). By default only future offerings are shown (equal to &#x60;?since&#x3D;&lt;today&gt;&#x60;).</param>
-    /// <param name="until">Filter all offerings by providing a maximum end date for the corresponding academic session, RFC3339 (full-date).</param>
-    /// <param name="sort">
-    ///Default: ["startDate"]<br/>
-    ///Items Enum: "offeringId" "name" "startDate" "endDate" "-offeringId" "-name" "-startDate" "-endDate"<br/>
-    ///Example: sort=offeringId,-endDate<br/>
-    /// Sort by one or more attributes, the default is ascending. Prefixing the attribute with a minus sign &#x60;-&#x60; allows for descending sort. Examples: [ATTR | -ATTR | ATTR1,-ATTR2]</param>
-    /// <response code="200">OK</response>
+    ///  <summary>
+    ///  GET /courses/{courseId}/offerings
+    ///  </summary>
+    ///  <remarks>Get a list of all offerings for this course, ordered chronologically.</remarks>
+    ///  <param name="courseId">Course ID</param>
+    ///  <param name="filterParams"></param>
+    ///  <param name="pagingParams"></param>
+    ///  <param name="teachingLanguage">Filter by teachingLanguage, which is a string describing the main teaching language, should be a three-letter language code as specified by ISO 639-2.</param>
+    ///  <param name="modeOfDelivery">Filter by modeOfDelivery</param>
+    ///  <param name="resultExpected">Filter by resultExpected</param>
+    ///  <param name="since">Filter all offerings by providing a minimum start date for the corresponding academic session, RFC3339 (full-date). By default only future offerings are shown (equal to &#x60;?since&#x3D;&lt;today&gt;&#x60;).</param>
+    ///  <param name="until">Filter all offerings by providing a maximum end date for the corresponding academic session, RFC3339 (full-date).</param>
+    ///  <param name="sort">
+    /// Default: ["startDate"]<br/>
+    /// Items Enum: "offeringId" "name" "startDate" "endDate" "-offeringId" "-name" "-startDate" "-endDate"<br/>
+    /// Example: sort=offeringId,-endDate<br/>
+    ///  Sort by one or more attributes, the default is ascending. Prefixing the attribute with a minus sign &#x60;-&#x60; allows for descending sort. Examples: [ATTR | -ATTR | ATTR1,-ATTR2]</param>
+    ///  <param name="cancellationToken"></param>
+    ///  <response code="200">OK</response>
     [HttpGet]
     [Route("courses/{courseId}/offerings")]
     [ValidateModelState]
     [SwaggerOperation("CoursesCourseIdOfferingsGet")]
     [SwaggerResponse(statusCode: 200, type: typeof(CourseOfferings), description: "OK")]
-    public virtual IActionResult CoursesCourseIdOfferingsGet([FromRoute][Required] Guid courseId, [FromQuery] FilterParams filterParams, [FromQuery] PagingParams pagingParams, [FromQuery] string? teachingLanguage, [FromQuery] List<string>? modeOfDelivery, [FromQuery] bool? resultExpected, [FromQuery] DateTime? since, [FromQuery] DateTime? until, [FromQuery] string? sort = "startDate")
+    public virtual async Task<IActionResult> CoursesCourseIdOfferingsGetAsync(
+        [FromRoute][Required] Guid courseId,
+        [FromQuery] FilterParams filterParams,
+        [FromQuery] PagingParams pagingParams,
+        [FromQuery] string? teachingLanguage,
+        [FromQuery] List<string>? modeOfDelivery,
+        [FromQuery] bool? resultExpected,
+        [FromQuery] DateTime? since,
+        [FromQuery] DateTime? until,
+        [FromQuery] string? sort = "startDate",
+        CancellationToken cancellationToken = default)
     {
         return BadRequest(new ErrorResponse(400, "Not implemented yet."));
     }
 
-    /// <summary>
-    /// GET /courses
-    /// </summary>
-    /// <remarks>Get a list of all courses, ordered by name (ascending).</remarks>
-    /// <param name="primaryCodeParam"></param>
-    /// <param name="filterParams"></param>
-    /// <param name="pagingParams"></param>
-    /// <param name="teachingLanguage">Filter by teachingLanguage, which is a string describing the main teaching language, should be a three-letter language code as specified by ISO 639-2.</param>
-    /// <param name="level">Filter by level</param>
-    /// <param name="modeOfDelivery">Filter by modeOfDelivery</param>
-    /// <param name="sort">
-    ///Default: ["name"]<br/>
-    ///Items Enum: "courseId" "name" "-courseId" "-name"<br/>
-    ///Example: sort=name,-courseId<br/>
-    /// Sort by one or more attributes, the default is ascending. Prefixing the attribute with a minus sign &#x60;-&#x60; allows for descending sort. Examples: [ATTR | -ATTR | ATTR1,-ATTR2]</param>
-    /// <response code="200">OK</response>
+    ///  <summary>
+    ///  GET /courses
+    ///  </summary>
+    ///  <remarks>Get a list of all courses, ordered by name (ascending).</remarks>
+    ///  <param name="primaryCodeParam"></param>
+    ///  <param name="filterParams"></param>
+    ///  <param name="pagingParams"></param>
+    ///  <param name="teachingLanguage">Filter by teachingLanguage, which is a string describing the main teaching language, should be a three-letter language code as specified by ISO 639-2.</param>
+    ///  <param name="level">Filter by level</param>
+    ///  <param name="modeOfDelivery">Filter by modeOfDelivery</param>
+    ///  <param name="sort">
+    /// Default: ["name"]<br/>
+    /// Items Enum: "courseId" "name" "-courseId" "-name"<br/>
+    /// Example: sort=name,-courseId<br/>
+    ///  Sort by one or more attributes, the default is ascending. Prefixing the attribute with a minus sign &#x60;-&#x60; allows for descending sort. Examples: [ATTR | -ATTR | ATTR1,-ATTR2]</param>
+    ///  <param name="cancellationToken"></param>
+    ///  <response code="200">OK</response>
     [HttpGet]
     [Route("courses")]
     [ValidateModelState]
     [SwaggerOperation("CoursesGet")]
     //[SwaggerResponse(statusCode: 200, type: typeof(MyPagination<Course>), description: "OK")]
     [SwaggerResponse(statusCode: 200, type: typeof(Courses), description: "OK")]
-    public virtual IActionResult CoursesGet([FromQuery] PrimaryCodeParam? primaryCodeParam, [FromQuery] FilterParams? filterParams, [FromQuery] PagingParams? pagingParams, [FromQuery] string? teachingLanguage, [FromQuery] string? level, [FromQuery] List<string>? modeOfDelivery, [FromQuery] string? sort = "name")
+    public virtual async Task<IActionResult> CoursesGetAsync(
+        [FromQuery] PrimaryCodeParam? primaryCodeParam,
+        [FromQuery] FilterParams? filterParams,
+        [FromQuery] PagingParams? pagingParams,
+        [FromQuery] string? teachingLanguage,
+        [FromQuery] string? level,
+        [FromQuery] List<string>? modeOfDelivery,
+        [FromQuery] string? sort = "name",
+        CancellationToken cancellationToken = default)
     {
         var parameters = new DataRequestParameters(primaryCodeParam, filterParams, pagingParams, sort);
-        var result = _coursesService.GetAllAsync(parameters);
+        var result = await _coursesService.GetAllAsync(parameters, cancellationToken);
         return Ok(result);
     }
 }
