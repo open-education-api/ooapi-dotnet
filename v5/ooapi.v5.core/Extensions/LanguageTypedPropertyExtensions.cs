a6d11c8f01a22889ee51b9e0268d276682600f9f
Kevin Hagenaars <Kevin.Hagenaars@han.nl>
2023-12-07 18:09:48 +0100

    Added attribute migrations

--- v5/ooapi.v5.core/Extensions/LanguageTypedPropertyExtensions.cs
+++ v5/ooapi.v5.core/Extensions/LanguageTypedPropertyExtensions.cs
@@ -1,4 +1,5 @@
-﻿using ooapi.v5.Models;
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using ooapi.v5.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,12 +11,12 @@ namespace ooapi.v5.core.Extensions
     public static class LanguageTypedPropertyExtensions
     {
         public static List<LanguageTypedString> ExtractStringsByPropertyName(this IEnumerable<LanguageTypedProperty> properties, string propertyName) { 
-            return properties.Where(x => x.PropertyName.Equals(propertyName))
+            return properties?.Where(x => x.PropertyName.Equals(propertyName))
                              .Select(x => new LanguageTypedString()
                              {
                                  Language = x.Language,
                                  Value = x.Value
-                             }).ToList();
+                             }).ToList() ?? new List<LanguageTypedString>();
         }
     }
 }
---
+++ v5/ooapi.v5.core/Migrations/20231207164130_FixSpecAndAttributesForEduSpecAndProgram.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ooapi.v5.core.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixSpecAndAttributesForEduSpecAndProgram : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Attributes_EducationSpecifications_EducationSpecificationId",
+                schema: "ooapiv5",
+                table: "Attributes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Attributes_Programs_ProgramId",
+                schema: "ooapiv5",
+                table: "Attributes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Attributes_EducationSpecificationId",
+                schema: "ooapiv5",
+                table: "Attributes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Attributes_ProgramId",
+                schema: "ooapiv5",
+                table: "Attributes");
+
+            migrationBuilder.DropColumn(
+                name: "EducationSpecificationId",
+                schema: "ooapiv5",
+                table: "Attributes");
+
+            migrationBuilder.DropColumn(
+                name: "ProgramId",
+                schema: "ooapiv5",
+                table: "Attributes");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProgramType",
+                schema: "ooapiv5",
+                table: "Programs",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PrimaryCodeType",
+                schema: "ooapiv5",
+                table: "Programs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PrimaryCode",
+                schema: "ooapiv5",
+                table: "Programs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Attributes",
+                schema: "ooapiv5",
+                table: "Programs",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Attributes",
+                schema: "ooapiv5",
+                table: "EducationSpecifications",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Attributes",
+                schema: "ooapiv5",
+                table: "Programs");
+
+            migrationBuilder.DropColumn(
+                name: "Attributes",
+                schema: "ooapiv5",
+                table: "EducationSpecifications");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProgramType",
+                schema: "ooapiv5",
+                table: "Programs",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PrimaryCodeType",
+                schema: "ooapiv5",
+                table: "Programs",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PrimaryCode",
+                schema: "ooapiv5",
+                table: "Programs",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "EducationSpecificationId",
+                schema: "ooapiv5",
+                table: "Attributes",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ProgramId",
+                schema: "ooapiv5",
+                table: "Attributes",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attributes_EducationSpecificationId",
+                schema: "ooapiv5",
+                table: "Attributes",
+                column: "EducationSpecificationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attributes_ProgramId",
+                schema: "ooapiv5",
+                table: "Attributes",
+                column: "ProgramId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Attributes_EducationSpecifications_EducationSpecificationId",
+                schema: "ooapiv5",
+                table: "Attributes",
+                column: "EducationSpecificationId",
+                principalSchema: "ooapiv5",
+                principalTable: "EducationSpecifications",
+                principalColumn: "EducationSpecificationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Attributes_Programs_ProgramId",
+                schema: "ooapiv5",
+                table: "Attributes",
+                column: "ProgramId",
+                principalSchema: "ooapiv5",
+                principalTable: "Programs",
+                principalColumn: "ProgramId");
+        }
+    }
+}
